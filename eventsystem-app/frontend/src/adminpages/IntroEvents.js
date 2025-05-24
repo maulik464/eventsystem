d9cc45298d57306990858a3c48bb2cd6f7eaf592
Maulik Dholariya <mmdholariya1998@gmail.com>
2025-05-24 22:39:10 +0530

    post entro events

--- eventsystem-app/frontend/src/adminpages/IntroEvents.js
+++ eventsystem-app/frontend/src/adminpages/IntroEvents.js
@@ -1,137 +1,98 @@
-import React, { useState, useEffect } from "react";
-import { CssBaseline, Container, Typography, TextField,Dialog,Stack } from "@mui/material";
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import FormControl from '@mui/joy/FormControl';
-import FormLabel from '@mui/joy/FormLabel';
+import React, { useState } from "react";
+import { CssBaseline, Container, Typography, TextField, Box, Button, FormControl, FormLabel } from "@mui/material";
 import Textarea from '@mui/joy/Textarea';
-import AddIcon from "@mui/icons-material/Add";
+import axios from 'axios';
 
 function IntroEvents() {
-    const [maxWidth, setMaxWidth] = useState("md");
-    const [fullWidth, setFullWidth] = useState(true);
-    const [open, setOpen] = useState(false);
+    const [data, setData] = useState({});
 
-    const handleClickOpen = () => {
-    setOpen(true);
-    // setIsEditMode(false);
-  };
+    const handleInput = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value });
+    }
 
-    const handleClose = () => {
-    setOpen(false);
-    // setIsEditMode(false);
-  };
+    const handleSubmit = async (e) => {
+        try {
+            const res = await axios.post('http://localhost:5000/introevents/createIntroEvent', data);
+            console.log("Create Response", res);
+        } catch (error) {
+            console.error("Error submitting data:", error);
+        }
+    }
+
     return (
         <div>
-            <Dialog
-                    open={open}
-                    maxWidth={maxWidth}
-                    fullWidth={fullWidth}
-                    onClose={handleClose}
-                  >
-            <React.Fragment>
-                <CssBaseline />
-                <Container
+            <CssBaseline />
+            <Container
+                sx={{
+                    width: "770px",
+                    boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+                    marginTop: "70px",
+                    marginLeft: "300px"
+                }}
+            >
+                <Box
                     sx={{
-                        width: "900px",
-                        boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-                        // marginLeft: '180px',
-                        // marginTop: "100px"
+                        bgcolor: "white",
+                        border: "2px solid white",
+                        width: "740px",
                     }}
                 >
-                    <Box
+                    <Typography
+                        variant="h4"
                         sx={{
-                            bgcolor: "white",
-                            border: "2px solid white",
-                            width: "900px",
-                            marginInlineStart:'-24px'
+                            fontWeight: 600,
+                            marginTop: "10px",
+                            textAlign: "center",
+                            color: "#211C84",
                         }}
                     >
-                        <Typography
-                            variant="h4"
+                        Add Content
+                    </Typography>
+                    <FormControl>
+                        <FormLabel>Your comment</FormLabel>
+                        <Textarea
+                            placeholder="Type something here…"
+                            minRows={3}
+                            name="comment"
+                            onChange={handleInput}
                             sx={{
-                                fontWeight: 600,
-                                marginTop: "40px",
-                                textAlign: "center",
-                                color: "#211C84",
+                                width: "400px",
+                                marginTop: "20px",
+                            }}
+                        />
+                        <TextField
+                            label="Enter Youtube Link"
+                            variant="outlined"
+                            fullWidth
+                            name="youtubelink"
+                            onChange={handleInput}
+                            sx={{
+                                width: '400px',
+                                marginTop: '30px',
+                                margin: '20px 0',
+                            }}
+                        />
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleSubmit}
+                            fullWidth
+                            sx={{
+                                height: '40px',
+                                fontSize: '14px',
+                                marginTop: '30px',
+                                margin: '20px',
+                                backgroundColor: "#211C84",
+                                marginLeft:"0px"
                             }}
                         >
-                            Add Content
-                        </Typography>
-                        <FormControl>
-                            <FormLabel>Your comment</FormLabel>
-                            <Textarea
-                                placeholder="Type something here…"
-                                minRows={3}
-                                endDecorator={
-                                    <Box
-                                        sx={{
-                                            width:'250px',
-                                            display: 'flex',
-                                            gap: 'var(--Textarea-paddingBlock)',
-                                            pt: 'var(--Textarea-paddingBlock)',
-                                            flex: 'auto'
-                                        }}
-                                    >
-                                    </Box>
-                                }
-                            />
-                            <TextField
-                                label="Enter Youtube Link"
-                                variant="outlined"
-                                fullWidth
-                                name="youtubelink"
-                                //   value={textField}
-                                //   onChange={handleTextChange}
-                                sx={{
-
-                                    width: '350px',
-                                    marginTop: '30px',
-                                    margin: '20px'
-                                }}
-                            />
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                //   onClick={handleSubmit}
-                                fullWidth
-                                sx={{
-                                    height: '40px',
-                                    fontSize: '14px',
-                                    marginTop: '30px',
-                                    margin: '20px',
-                                    width: '350px',
-                                    color:'white',
-                                    backgroundColor: "#211C84",
-                                }}
-                            >
-                                Submit
-                            </Button>
-                        </FormControl>
-                    </Box>
-                </Container>
-            </React.Fragment>
-        </Dialog>
-        <Stack sx={{ margin: "20px", marginLeft: "200px",marginTop:'50px' }}>
-            <Button
-              variant="contained"
-              sx={{
-                height: "50px",
-                width: "200px",
-                fontSize: 20,
-                backgroundColor: "#211C84",
-                marginTop:'20px',
-                color:'white'
-              }}
-              onClick={() => {
-                handleClickOpen();
-              }}
-            >
-              <AddIcon />add Content
-            </Button>
-          </Stack>
+                            Submit Content
+                        </Button>
+                    </FormControl>
+                </Box>
+            </Container>
         </div>
-    )
+    );
 }
 
-export default IntroEvents
+export default IntroEvents;
